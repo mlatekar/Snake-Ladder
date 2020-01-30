3e333d53ca57c572a0173b09937b8e2d7d6d97d0
mangesh <mlatekar@gmail.com>
2020-01-30 16:37:09 +0530

    ADDED:Repeat till position 100 & rollDie()for random die no

--- snakeladder.sh
+++ snakeladder.sh
@@ -1,10 +1,19 @@
 echo "Welcome to Snake and Ladder Game"
+STARTINGPOSITION=0
 singlePlayerPosition=0
+WINNINGPOSITION=100
+function rollDie()
+{
 dieNumber=$(($((RANDOM%6))+1))
-echo "Rolls the die :" $dieNumber
-
+echo "Rolls the die : "$dieNumber
+}
 function newPosition()
 {
+
+	#rollDie
+while [[ $singlePlayerPosition -lt $WINNINGPOSITION ]]
+do
+	rollDie
 	case $((RANDOM%3)) in
 		0)
 		singlePlayerPosition=$singlePlayerPosition
@@ -14,9 +23,13 @@ function newPosition()
 		;;
 		2)
 		singlePlayerPosition=$(($singlePlayerPosition-$dieNumber))
-		;;
+    		if [ $singlePlayerPosition -lt 0 ]
+	   	  then
+		   	singlePlayerPosition=$STARTINGPOSITION
+		   fi
+	   ;;
 	esac
 echo "New position of player : " $singlePlayerPosition
+done
 }
 newPosition
-
