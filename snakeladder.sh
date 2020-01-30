f7d1d77c4b1fa656f0b8097d55e7a542f3094e84
mangesh <mlatekar@gmail.com>
2020-01-30 14:11:47 +0530

    ADDED:newposition()that player play using RANDOM

--- snakeladder.sh
+++ snakeladder.sh
@@ -2,3 +2,21 @@ echo "Welcome to Snake and Ladder Game"
 singlePlayerPosition=0
 dieNumber=$(($((RANDOM%6))+1))
 echo "Rolls the die :" $dieNumber
+
+function newPosition()
+{
+	case $((RANDOM%3)) in
+		0)
+		singlePlayerPosition=$singlePlayerPosition
+		;;
+		1)
+		singlePlayerPosition=$(($singlePlayerPosition+$dieNumber))
+		;;
+		2)
+		singlePlayerPosition=$(($singlePlayerPosition-$dieNumber))
+		;;
+	esac
+echo "New position of player : " $singlePlayerPosition
+}
+newPosition
+
