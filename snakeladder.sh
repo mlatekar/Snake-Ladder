6ef573fcd8f1b02f1a96b363e8d6cc188db17d00
mangesh <mlatekar@gmail.com>
2020-02-03 15:11:41 +0530

    ADDED:player2 and display winner

--- snakeladder.sh
+++ snakeladder.sh
@@ -1,46 +1,99 @@
+#!/bin/bash -x
 echo "Welcome to Snake and Ladder Game"
+
+#constant
 STARTINGPOSITION=0
-singlePlayerPosition=0
 WINNINGPOSITION=100
+NOPLAY=0
+LADDER=1
+SNAKE=2
+
+#variables
 noOfDicePlayed=0
+singlePlayerPosition=$STARTINGPOSITION
+firstPlayer=$STARTINGPOSITION
+secondPlayer=$STARTINGPOSITION
+flag=0
+
+#declare dictionay
 declare -A dicePlayed
-function rollDie()
-{
-dieNumber=$(($((RANDOM%6))+1))
-echo "Rolls the die : "$dieNumber
-((noOfDicePlayed++))
+
+
+function switchPlayer()  #switch the player1 and player2
+{ 
+	if [[ $flag -eq 0 ]]
+	then 
+		noOfDicePlayed=$(( noOfDicePlayed+1 ))
+		singlePlayerPosition=$firstPlayer
+		player=1
+		rollDie
+		firstPlayer=$singlePlayerPosition
+		flag=1
+	else
+		secondPlayer=$singlePlayerPosition
+		singlePlayerPosition=$secondPlayer
+		player=2
+      rollDie
+      secondPlayer=$singlePlayerPosition
+      flag=0
+	fi
 }
+
+function rollDie()  # genrate number 1 to 6
+{
+	dieNumber=$(($((RANDOM%6))+1))
+	echo "Rolls the die : "$dieNumber
+	option	 #calling option function 
+}
+
+function option()  
+{
+	case $((RANDOM%3)) in
+		$NOPLAY)
+					singlePlayerPosition=$singlePlayerPosition
+					;;
+
+		$LADDER)
+					singlePlayerPosition=$(($singlePlayerPosition+$dieNumber))
+						if [[ $singlePlayerPosition -gt $WINNINGPOSITION ]]
+			  				then
+								singlePlayerPosition=$((singlePlayerPosition-dieNumber))
+		   			fi
+					;;
+
+		$SNAKE)
+					singlePlayerPosition=$(($singlePlayerPosition-$dieNumber))
+		 				if [ $singlePlayerPosition -lt $STARTINGPOSITION ]
+	   	  				then
+		   					singlePlayerPosition=$STARTINGPOSITION
+		 				fi
+					;;
+   esac
+	position
+
+   echo "New position of player : " $singlePlayerPosition
+}
+
+function position()    #player position store 
+{
+	dicePlayed[Player_"$player"_"$noOfDicePlayed"]=$singlePlayerPosition
+}
+
 function newPosition()
 {
-	#rollDie
-while [[ $singlePlayerPosition -lt $WINNINGPOSITION ]]
-do
-	rollDie
-	case $((RANDOM%3)) in
-		0)
-		singlePlayerPosition=$singlePlayerPosition
-		;;
-		1)
-		previousPosition=$singlePlayerPosition
-		singlePlayerPosition=$(($singlePlayerPosition+$dieNumber))
-			if [[ $singlePlayerPosition -gt $WINNINGPOSITION ]]
-			  then
-				singlePlayerPosition=$previousPosition
-			  fi
-		;;		2)
-		singlePlayerPosition=$(($singlePlayerPosition-$dieNumber))
-		    	
-		if [ $singlePlayerPosition -lt 0 ]
-	   	  then
-		   	singlePlayerPosition=$STARTINGPOSITION
-		 fi
-		;;
-esac
-		dicePlayed[$noOfDicePlayed]=$singlePlayerPosition
-		echo ${dicePlayed[noOfDicePlayed]}
-	
-echo "New position of player : " $singlePlayerPosition
-echo "dice roll time : " ${dicePlayed[@]}
-done
+   while [[ $singlePlayerPosition -ne $WINNINGPOSITION ]]
+   	do
+	  		 switchPlayer
+   	done
+}
+function winPlayer()
+{
+	if [[ $firstPlayer -gt $secondPlayer ]]
+		then
+			echo "player 1 win"
+	else
+		echo "Player 2 win"
+	fi
 }
 newPosition
+winPlayer
